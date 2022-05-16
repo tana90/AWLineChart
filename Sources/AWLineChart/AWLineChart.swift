0be376e909b9a581cf8e7db9db0060631af9c813
Tudor Octavian ANA <iamtana90@gmail.com>
2022-05-16 17:31:56 +0300

    Dummy commit

--- Sources/AWLineChart/AWLineChart.swift
+++ Sources/AWLineChart/AWLineChart.swift
@@ -562,16 +562,3 @@ extension AWLineChart {
         }
     }
 }
-
-// MARK: Array extension
-
-extension Array {
-    
-    fileprivate func pick(length: Int) -> [Element] {
-        precondition(length >= 0, "length must not be negative")
-        if length >= count { return self }
-        let oldMax = Double(count - 1)
-        let newMax = Double(length - 1)
-        return (0..<length).map { self[Int((Double($0) * oldMax / newMax).rounded())] }
-    }
-}
